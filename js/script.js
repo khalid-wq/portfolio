4a4276e55b364c58b68fa82cc07a1d655c86bd8f
khalid ismail <khalidsmail50@gmail.com>
2022-12-25 19:09:43 +0100

    new

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-var typed = new Typed(".typing",{
-    strings:["Web Designer","Web Devloper","Graphic Designer","Game Devloper"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
+var typed = new Typed(".typing", {
+    strings: ["Web Designer", "Web Devloper", "Graphic Designer", "Game Devloper"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
 })
+// google
